d246c3c55fd7d07115bcdcdfdff4cae42d83f884
helenaponted <helenaponte@hotmail.es>
2023-08-22 15:34:54 +0100

    cambios-tabla

--- backend/views/coders/getAllCoders.php
+++ backend/views/coders/getAllCoders.php
@@ -37,7 +37,7 @@
                     Telefono
                 </th> 
                 <th scope="col" class="px-6 py-3">
-                    Acciones
+                    Ciudad
                 </th>
             </tr>
         </thead>
@@ -58,6 +58,7 @@
                 echo "<td>" .$row["surname2"] . "</td>";
                 echo "<td>" .$row["email"] . "</td>";
                 echo "<td>" .$row["phone"] . "</td>";
+                echo "<td>" .$row["city"] . "</td>";
                 echo "<td>
                     <a href='editCoder.php?id=" . $row["id"] . "'>
                         <button type='button' class='text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700'>
