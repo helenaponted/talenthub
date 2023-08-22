93864631fe821dc2daa2303eeb63d0e15024cf10
Alba Velasco <albavelascoglez@gmail.com>
2023-08-22 14:17:49 +0200

    BootcampLanding done

---
+++ backend/views/landingPage/LandingPageBootcamp.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css" rel="stylesheet">
+    <title>Bootcamp Page</title>
+    <link rel="stylesheet" href="./../bootcamp/bootcamp.css">
+</head>
+<body class="fondo">
+ 
+  <div class="container mx-auto p-12 bg-gray-100 rounded-xl mt-10">
+    <h1 class="text-4xl uppercase font-bold from-current mb-8 text-center">Bootcamps</h1>
+   
+    <div class="sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-4 space-y-4 sm:space-y-0">
+      <div class="bg-white">
+        <div>
+          <div class="shadow-lg hover:shadow-xl transform transition duration-500 hover:scale-105">
+            <div>
+              <img class="w-full" src="./../../../public/Fem.jpg" />
+              <div class="px-4 py-2">
+                <h1 class="text-xl font-gray-700 font-bold">FemCoders Norte</h1>
+                <div class="flex space-x-2 mt-2">
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                  </span>
+                  <h3 class="text-lg text-gray-600 font-semibold mb-2">Zona Norte</h3>
+                </div>
+                <button class="mt-12 w-full text-center bg-red-400 py-2 rounded-lg">Entrar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+ 
+      <div class="bg-white">
+        <div>
+          <div class="shadow-lg hover:shadow-xl transform transition duration-500 hover:scale-105">
+            <div>
+              <img class="w-full" src="./../../../public/digital.avif" />
+              <div class="px-4 py-2">
+                <h1 class="text-xl font-gray-700 font-bold">Digital Academy</h1>
+                <div class="flex space-x-2 mt-2">
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                  </span>
+                  <h3 class="text-lg text-gray-600 font-semibold mb-2">Langreo</h3>
+                </div>
+                <button class="mt-12 w-full text-center bg-red-400 py-2 rounded-lg">Entrar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="bg-white">
+        <div>
+          <div class="shadow-lg hover:shadow-xl transform transition duration-500 hover:scale-105">
+            <div>
+              <img class="w-full" src="./../../../public/unique.png" />
+              <div class="px-4 py-2">
+                <h1 class="text-xl font-gray-700 font-bold">Unique</h1>
+                <div class="flex space-x-2 mt-2">
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                  </span>
+                  <h3 class="text-lg text-gray-600 font-semibold mb-2">Asturias</h3>
+                </div>
+              
+                <button class="mt-12 w-full text-center bg-red-400 py-2 rounded-lg">Entrar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Box-3 -->
+      <div class="bg-white">
+        <div>
+          <div class="shadow-lg hover:shadow-xl transform transition duration-500 hover:scale-105">
+            <div>
+              <img class="w-full" src="./../../../public/rural.png" />
+              <div class="px-4 py-2">
+                <h1 class="text-xl font-gray-700 font-bold">Rural Camp</h1>
+                <div class="flex space-x-2 mt-2">
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-yellow-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                    </svg>
+                  </span>
+                  <h3 class="text-lg text-gray-600 font-semibold mb-2">Zonas Rurales</h3>
+                </div>
+                <button class="mt-12 w-full text-center bg-red-400 py-2 rounded-lg">Entrar</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
