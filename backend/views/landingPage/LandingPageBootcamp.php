521bc68271190b8c9d81ea4716f894d909bcc0db
Alba Velasco <albavelascoglez@gmail.com>
2023-08-22 15:18:28 +0200

    LandingBootcampPage with nav responsive

--- backend/views/landingPage/LandingPageBootcamp.php
+++ backend/views/landingPage/LandingPageBootcamp.php
@@ -5,11 +5,78 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css" rel="stylesheet">
     <title>Bootcamp Page</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="./../bootcamp/bootcamp.css">
+    <link rel="stylesheet" href="./../coders/addCoder.css">
+    <link rel="stylesheet" href="./../../styles.css">
 </head>
 <body class="fondo">
- 
-  <div class="container mx-auto p-12 bg-gray-100 rounded-xl mt-10">
+<div class="w-full relative flex ct-docs-disable-sidebar-content overflow-x-hidden">
+<aside class="w-56 bg-white h-screen fixed top-0 left-0 bottom-0 overflow-hidden border-r shadow-md">
+    <div class="logo flex items-center justify-center h-20 shadow-md mt-6 bg-secondary">
+      <img src="./../../../src/assets/logo-color.svg" alt="Logo" />
+    </div>
+    <ul class="py-4">
+      <li>
+        <a href="#" class="flex items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-orange-500 px-4 responsive-hidden">
+          <i class="fa-solid fa-house mr-2"></i>
+          <span class="text-sm font-medium ">Inicio</span>
+        </a>
+      </li>
+      <li class="group has-submenu">
+        <a href="#" class="flex items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-orange-500 px-4 responsive-hidden">
+          <i class="fa-solid fa-graduation-cap mr-2"></i>
+          <span class="text-sm font-medium ">Bootcamps</span>
+        </a>
+        <ul class="sub-menu ml-12 mt-2 space-y-1 bg-white border-l border-t border-b">
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">FemCoders Norte</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">FemCoders Barcelona</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">Unique</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">Rural Camp</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">Digital Academy</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">Añadir bootcamp</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#" class="flex items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-orange-500 px-4 responsive-hidden">
+          <i class="fa-solid fa-users mr-2"></i>
+          <span class="text-sm font-medium">Todos los coders</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="flex items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-orange-500 px-4 responsive-hidden">
+          <i class="fa-regular fa-clock mr-2"></i>
+          <span class="text-sm font-medium">Coders en reserva</span>
+        </a>
+      </li>
+      <li class="group staff">
+        <a href="#" class="flex items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-orange-500 px-4 responsive-hidden">
+          <i class="fa-solid fa-clipboard mr-2"></i>
+          <span class="text-sm font-medium">Staff</span>
+        </a>
+        <ul class="sub-menu ml-12 mt-2 space-y-1 bg-white border-l border-t border-b">
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">Formadoras</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">Coformadoras</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">Responsables Proyecto</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">Ver todo el staff</a></li>
+        </ul>
+      </li>
+    </ul>
+    <div class="px-4 py-2 mt-auto">
+      <div class="flex items-center space-x-2 config">
+        <a href="#" class="text-gray-500 hover:text-orange-500">
+          <i class="fas fa-cog text-lg"></i>
+        </a>
+        <a href="#" class="text-gray-500 hover:text-orange-500">
+          <i class="fas fa-sign-out-alt text-lg"></i>
+        </a>
+      </div>
+    </div>
+  </aside>
+  </div>
+
+  <div class="h-full ml-14 mt-14 mb-10 md:ml-64 sm:10">
+  <div class="container mx-auto p-12 bg-gray-100 rounded-xl mt-10 flex-1">
     <h1 class="text-4xl uppercase font-bold from-current mb-8 text-center">Bootcamps</h1>
    
     <div class="sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-4 space-y-4 sm:space-y-0">
@@ -107,6 +174,7 @@
       </div>
     </div>
   </div>
+</div>
 </div>
 </body>
 </html>
