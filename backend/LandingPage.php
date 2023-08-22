c2a98514c5c30880cbbd3f6b2fabb7d83030a5ed
Jimena <jimenacastellanos81@gmail.com>
2023-08-22 11:40:50 +0200

    Landing Page and footer done

---
+++ backend/LandingPage.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="" />
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Talenthub</title>
+  </head>
+  <body>
+  <div class="bg-cover bg-center  h-auto text-white py-24 px-10 object-fill m-5" style="background-image: url(./../public/coders.jpg)">
+       <div class="md:w-1/2">
+        
+        <p class="text-3xl font-bold">CODERS</p>
+        <p class="text-2xl mb-10 leading-none">Gestión de Coders</p>
+        <a href="#" class="bg-red-800 py-4 px-8 text-white font-bold uppercase text-xs rounded hover:bg-gray-200 hover:text-gray-800">ACCESO</a>
+        </div>
+    </div>
+</br>
+    <div class="bg-cover bg-center  h-auto text-white py-24 px-10 object-fill m-5" style="background-image: url(./../public/coding2.webp)">
+       <div class="md:w-1/2">
+        <p class="text-3xl font-bold">BOOTCAMPS</p>
+        <p class="text-2xl mb-10 leading-none">Gestión de Bootcamps</p>
+        <a href="#" class="bg-red-800 py-4 px-8 text-white font-bold uppercase text-xs rounded hover:bg-gray-200 hover:text-gray-800">ACCESO</a>
+        </div>
+    </div>
+</br>
+<div class="bg-cover bg-center  h-auto text-white py-24 px-10 object-fill m-5" style="background-image: url(./../public/staff.jpg)">
+       <div class="md:w-1/2">
+        <p class="text-3xl font-bold">STAFF</p>
+        <p class="text-2xl mb-10 leading-none">Gestión de Staff</p>
+        <a href="#" class="bg-red-800 py-4 px-8 text-white font-bold uppercase text-xs rounded hover:bg-gray-200 hover:text-gray-800">ACCESO</a>
+        </div>
+    </div>
+
+
+    
+    <footer class="bg-white dark:bg-gray-900">
+    <div class="container px-6 py-8 mx-auto">
+        <div class="flex flex-col items-center text-center">
+            <a href="#">
+                <img class="w-auto h-7" src="./../public/LogoF5Footer.png" alt="">
+            </a>
+            <p class="max-w-md mx-auto mt-4 text-gray-500 dark:text-gray-400">TalentHub</p>
+            
+        </div>
+        <hr class="my-10 border-gray-200 dark:border-gray-700" />
+        <div class="flex flex-col items-center sm:flex-row sm:justify-between">
+            <p class="text-sm text-gray-500">© Copyright 2023. All Rights Reserved.</p>
+            <div class="flex mt-3 -mx-2 sm:mt-0">
+                <a href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Teams </a>
+                <a href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Privacy </a>
+                <a href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Cookies </a>
+            </div>
+        </div>
+    </div>
+</footer>
+    
+  </body>
+</html>
+
+
+
+
+
+
+
