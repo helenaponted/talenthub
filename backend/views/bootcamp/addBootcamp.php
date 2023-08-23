c5608bebda750ffcd935b5c70cbfeb3831208488
Alba Velasco <albavelascoglez@gmail.com>
2023-08-23 12:42:29 +0200

    layout addBootcamp responsive done

--- backend/views/bootcamp/addBootcamp.php
+++ backend/views/bootcamp/addBootcamp.php
@@ -23,33 +23,128 @@ require_once __DIR__ . './../../vendor/autoload.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alta de nuevo Bootcamp</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css" rel="stylesheet">
-    <title>Talenthub</title>
-    <link rel="stylesheet" href="./bootcamp.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    
+    <link rel="stylesheet" href="./../../styles.css">
+    <link rel="stylesheet" href="./../coders/addCoder.css">
+    
+
 </head>
-
-<body class="grid h-screen place-items-center fondo">
-    <div class="bg-white p-8 rounded-lg shadow-md w-96 justify-center">
-    <h1 class="text-2xl font-semibold mb-4 text-secondary text-center">Añadir Bootcamp</h1>
-    <form action="./addBootcamp.php" method="POST">
-        <fieldset>
-            <label class="block mb-2" for="name">Nombre</label>
-            <input type="text" class="w-full border rounded-lg py-2 px-3 mb-2" name="name_bootcamp" required >
-            <label class="block mb-2" for="start">Empieza</label>
-            <input type="date" class="w-full border rounded-lg py-2 px-3 mb-2" name="start" required>
-            <label class="block mb-2" for="end">Acaba</label>
-            <input type="date" class="w-full border rounded-lg py-2 px-3 mb-2" name="end" required>
-            <label class="block mb-2" for="remote">¿Es remoto?</label>
-            <input type="text" class="w-full border rounded-lg py-2 px-3 mb-2" name="remote" value=0>
-            <div class="flex justify-center">
-                    <button type="submit" name="submit" class="bg-secondary text-white py-2 px-4 mt-2 rounded-lg hover:bg-red-600 transition duration-300 ease-in-out">AÑADIR</button>
-                </div>
-        </fieldset>
-    </form>
-    <a class="subtitulo mt-2" href="./indexBootcamp.php">
-
-        Volver a la lista
-  
-    </a>
+<body>
+<aside class="w-56 bg-white h-screen fixed top-0 left-0 bottom-0 overflow-hidden border-r shadow-md">
+    <div class="logo flex items-center justify-center h-20 shadow-md mt-6 bg-secondary">
+      <img src="./../../../src/assets/logo-color.svg" alt="Logo" />
     </div>
+    <ul class="py-4">
+      <li>
+        <a href="./../landingPage/LandingPage.php" class="flex items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-orange-500 px-4 responsive-hidden">
+          <i class="fa-solid fa-house mr-2"></i>
+          <span class="text-sm font-medium ">Inicio</span>
+        </a>
+      </li>
+      <li class="group has-submenu">
+        <a href="./../bootcamp/indexBootcamp.php" class="flex items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-orange-500 px-4 responsive-hidden">
+          <i class="fa-solid fa-graduation-cap mr-2"></i>
+          <span class="text-sm font-medium ">Bootcamps</span>
+        </a>
+        <ul class="sub-menu ml-12 mt-2 space-y-1 bg-white border-l border-t border-b">
+          <li><a href="./indexFemCodersNorte.php" class="block px-4 py-2 text-gray-500 hover:text-orange-500">FemCoders Norte</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">FemCoders Barcelona</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">Unique</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">Rural Camp</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">Digital Academy</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500"><i class="fa-solid fa-plus mr-2"></i>Añadir bootcamp</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="./getAllCoders.php" class="flex items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-orange-500 px-4 responsive-hidden">
+          <i class="fa-solid fa-users mr-2"></i>
+          <span class="text-sm font-medium">Todos los coders</span>
+        </a>
+      </li>
+      <li>
+        <a href="#" class="flex items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-orange-500 px-4 responsive-hidden">
+          <i class="fa-regular fa-clock mr-2"></i>
+          <span class="text-sm font-medium">Coders en reserva</span>
+        </a>
+      </li>
+      <li class="group has-submenu">
+        <a href="./../staff/indexStaff.php" class="flex items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-orange-500 px-4 responsive-hidden">
+          <i class="fa-solid fa-clipboard mr-2"></i>
+          <span class="text-sm font-medium">Staff</span>
+        </a>
+        <ul class="sub-menu ml-12 mt-2 space-y-1 bg-white border-l border-t border-b">
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">Formadoras</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">Coformadoras</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">Responsables Proyecto</a></li>
+          <li><a href="#" class="block px-4 py-2 text-gray-500 hover:text-orange-500">Ver todo el staff</a></li>
+        </ul>
+      </li>
+    </ul>
+    <div class="px-4 py-2 mt-auto">
+      <div class="flex items-center space-x-2 config">
+        <a href="#" class="text-gray-500 hover:text-orange-500">
+          <i class="fas fa-cog text-lg"></i>
+        </a>
+        <a href="#" class="text-gray-500 hover:text-orange-500">
+          <i class="fas fa-sign-out-alt text-lg"></i>
+        </a>
+      </div>
+    </div>
+  </aside>
+  <main class="h-full ml-14 mt-14 mb-10 md:ml-56 p-8  sm:10">
+    <h2 class="text-2xl font-semibold mb-4">Alta de nuevo Bootcamp</h2>
+        <form action="./rpaddCoder.php" method="POST">
+            <label for="name" class="block font-semibold">Nombre</label>
+            <input type="text" name="name" class="form-input" required>
+
+            <label for="start" class="block font-semibold">Empieza</label>
+            <input type="date" name="start" class="form-input" required>
+
+            <label for="end" class="block font-semibold">Acaba</label>
+            <input type="date" name="end" class="form-input" required>
+
+            <label for="remote" class="block font-semibold">¿Es Remoto?</label>
+            <select name="remote" class="form-select">
+                <option value="">-- Selecciona un Bootcamp --</option>
+                <option value=1>SIN DEFINIR</option>
+                <option value=2>REMOTO</option>
+                <option value=3>PRESENCIAL</option>           
+            </select>
+
+
+            <div class="text-center mt-6">
+                <button type="submit" name="submit" class="form-button">Registrar Bootcamp</button>
+            </div>
+        </form>
+    </div>
+
+    <footer class="bg-transparent dark:bg-gray-900 w-9/12 md:w-4/6  sm:w-2/3 fixed bottom-0">
+    <div class="footerContainer px-6 py-8 mx-auto">
+        <div class="flex flex-col items-center text-center">
+            <a href="#">
+                <img class="w-auto h-7" src="./../../../public/LogoF5Footer.png" alt="">
+            </a>
+            <p class="max-w-md mx-auto mt-4 text-gray-500 dark:text-gray-400">TalentHub</p>
+            
+        </div>
+        <hr class="my-10 border-gray-200 dark:border-gray-700" />
+        <div class="flex flex-col items-center sm:flex-row sm:justify-between">
+            <p class="text-sm text-gray-500">© Copyright 2023. All Rights Reserved.</p>
+            <div class="flex mt-3 -mx-2 sm:mt-0">
+                <a href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Teams </a>
+                <a href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Privacy </a>
+                <a href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Cookies </a>
+            </div>
+        </div>
+    </div>
+</footer>
+    
+  </main>
+  
+ 
+
 </body>
+</html>
--- backend/views/coders/RPaddCoder.php
+++ backend/views/coders/RPaddCoder.php
@@ -29,7 +29,7 @@ require_once __DIR__ . '/../../vendor/autoload.php';
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.7/dist/tailwind.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     
-    <link rel="stylesheet" href="./../../../styles.css">
+    <link rel="stylesheet" href="./../../styles.css">
     <link rel="stylesheet" href="addCoder.css">
     
 
