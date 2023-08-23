365198eddedeb7744e7e62d75df77f102a2a7615
anaencinasd <YOUR_EMAIL>
2023-08-23 22:08:40 +0200

    funciona el addcodertobootcamp

--- backend/views/coders/indexFemCodersNorte.php
+++ backend/views/coders/indexFemCodersNorte.php
@@ -78,7 +78,7 @@
     <a href="./RPaddCoder.php">
         <button  class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Crear nuevo coder</button>
     </a>
-    <a href="./indexAddCoderToBootcampfemcoders.php">
+    <a href="./indexAddCoderToBootcamp.php">
         <button  class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Añadir coder a este bootcamp</button>
     </a>
 </div>
