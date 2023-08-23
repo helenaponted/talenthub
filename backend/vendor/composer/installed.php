30b1f65e0bdc62d9bf241faf941415d13b258d0e
Jimena <jimenacastellanos81@gmail.com>
2023-08-23 14:22:07 +0200

    addStaff

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'helen/backend',
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'reference' => 'c5608bebda750ffcd935b5c70cbfeb3831208488',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'helen/backend' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'reference' => 'c5608bebda750ffcd935b5c70cbfeb3831208488',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
