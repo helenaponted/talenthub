59dbea860cb25852a1e5b05f26459a6f8c1d42f3
anaencinasd <YOUR_EMAIL>
2023-08-24 14:41:40 +0200

    repository ok

--- backend/views/staff/deleteStaff.php
+++ backend/views/staff/deleteStaff.php
@@ -31,9 +31,9 @@ require "./../../vendor/autoload.php";
 <div class="min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover"   id="modal-id">
     <div class="absolute bg-transparent opacity-80 inset-0 z-0"></div>
     <div class="w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-white ">
-      <!--content-->
+      
       <div class="">
-        <!--body-->
+       
         <div class="text-center p-5 flex-auto justify-center">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 -m-1 flex items-center text-red-500 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
@@ -43,7 +43,7 @@ require "./../../vendor/autoload.php";
 </svg>
                         <h2 class="text-xl font-bold py-4 ">El coder se ha eliminado correctamente</h2>
         </div>
-        <!--footer-->
+        
         <div class="p-3  mt-2 text-center space-x-4 md:block">
             <a href="indexStaff.php" >
             <button class="mb-2 md:mb-0 bg-black px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-200 rounded-full hover:shadow-lg hover:bg-red-800">
