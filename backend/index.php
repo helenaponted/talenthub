067639de776a449fed1d4b48ecf77ec8d0d30fde
NINETTHE <raissa.escobar1408@gmail.com>
2023-08-24 18:57:49 +0200

    path to add bootcamp defined in landing page

--- backend/index.php
+++ backend/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="./../../styles.css">
+    <link rel="stylesheet" href="./styles.css">
     <style>
        
     </style>
